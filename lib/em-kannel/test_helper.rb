2c660945f66bbe7946edf9fc08dd4a4a6691b3e4
John Pignata <john@pignata.com>
2012-01-22 16:24:45 -0500

    Add fake guid to test helper response

--- lib/em-kannel/test_helper.rb
+++ lib/em-kannel/test_helper.rb
@@ -8,8 +8,13 @@ module EventMachine
 
     Client.class_eval do
       def fake_response
+        guid = UUID.new.generate
+
         status = OpenStruct.new(status: 202)
-        http   = OpenStruct.new(response_header: status)
+        http   = OpenStruct.new(
+          response_header: status,
+          response: "0: Accepted for delivery: #{guid}"
+        )
 
         Response.new(http, Time.now)
       end
